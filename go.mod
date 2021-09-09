b8e0a7c472d0f7fb5bcf675d4a176b64a992b791
rzry <1319339638@qq.com>
2021-09-09 10:25:00 +0800

    feat:log
    Signed-off-by: rzry <1319339638@qq.com>

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/rzry/kwstart
+
+go 1.17
+
+require (
+	github.com/marmotedu/component-base v1.0.1
+	github.com/rzry/airdrop v0.0.1
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v1.0.5
+	go.uber.org/zap v1.19.1
+	k8s.io/klog/v2 v2.20.0
+)
+
+require (
+	github.com/go-logr/logr v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+)
---
+++ kwlog/context.go
@@ -0,0 +1,32 @@
+package kwlog
+
+import (
+	"context"
+)
+
+type key int
+
+const (
+	logContextKey key = iota
+)
+
+// WithContext returns a copy of context in which the kwlog value is set.
+func WithContext(ctx context.Context) context.Context {
+	return std.WithContext(ctx)
+}
+
+func (l *zapLogger) WithContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, logContextKey, l)
+}
+
+// FromContext returns the value of the kwlog key on the ctx.
+func FromContext(ctx context.Context) Logger {
+	if ctx != nil {
+		logger := ctx.Value(logContextKey)
+		if logger != nil {
+			return logger.(Logger)
+		}
+	}
+
+	return WithName("Unknown-Context")
+}
