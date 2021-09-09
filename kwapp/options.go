c5b2cd6d28c3aa6d33dab6dd6f5b98db99afb290
rzry <1319339638@qq.com>
2021-09-09 16:04:25 +0800

    fix:package
    Signed-off-by: rzry <1319339638@qq.com>

--- kwapp/options.go
+++ kwapp/options.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-package app
+package kwapp
 
 import (
 	cliflag "github.com/marmotedu/component-base/pkg/cli/flag"
--- kwdb/plugin.go
+++ kwdb/plugin.go
@@ -1,4 +1,4 @@
-package db
+package kwdb
 
 import (
 	"time"
