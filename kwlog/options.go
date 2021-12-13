70e1cd9b36bcc557efb481d307a3c817580225eb
rzry <1319339638@qq.com>
2021-12-13 14:10:21 +0800

    fix:reset import
    Signed-off-by: rzry <1319339638@qq.com>

--- kwlog/options.go
+++ kwlog/options.go
@@ -1,144 +1,144 @@
 package kwlog
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/marmotedu/component-base/pkg/json"
-	"github.com/spf13/pflag"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+    "fmt"
+    "strings"
+    
+    "github.com/rzry/kwstart/pkg/json"
+    "github.com/spf13/pflag"
+    "go.uber.org/zap"
+    "go.uber.org/zap/zapcore"
 )
 
 const (
-	flagLevel             = "kwlog.level"
-	flagDisableCaller     = "kwlog.disable-caller"
-	flagDisableStacktrace = "kwlog.disable-stacktrace"
-	flagFormat            = "kwlog.format"
-	flagEnableColor       = "kwlog.enable-color"
-	flagOutputPaths       = "kwlog.output-paths"
-	flagErrorOutputPaths  = "kwlog.error-output-paths"
-	flagDevelopment       = "kwlog.development"
-	flagName              = "kwlog.name"
-
-	consoleFormat = "console"
-	jsonFormat    = "json"
+    flagLevel             = "kwlog.level"
+    flagDisableCaller     = "kwlog.disable-caller"
+    flagDisableStacktrace = "kwlog.disable-stacktrace"
+    flagFormat            = "kwlog.format"
+    flagEnableColor       = "kwlog.enable-color"
+    flagOutputPaths       = "kwlog.output-paths"
+    flagErrorOutputPaths  = "kwlog.error-output-paths"
+    flagDevelopment       = "kwlog.development"
+    flagName              = "kwlog.name"
+    
+    consoleFormat = "console"
+    jsonFormat    = "json"
 )
 
 // Options contains configuration items related to kwlog.
 type Options struct {
-	OutputPaths       []string `json:"output-paths"       mapstructure:"output-paths"`
-	ErrorOutputPaths  []string `json:"error-output-paths" mapstructure:"error-output-paths"`
-	Level             string   `json:"level"              mapstructure:"level"`
-	Format            string   `json:"format"             mapstructure:"format"`
-	DisableCaller     bool     `json:"disable-caller"     mapstructure:"disable-caller"`
-	DisableStacktrace bool     `json:"disable-stacktrace" mapstructure:"disable-stacktrace"`
-	EnableColor       bool     `json:"enable-color"       mapstructure:"enable-color"`
-	Development       bool     `json:"development"        mapstructure:"development"`
-	Name              string   `json:"name"               mapstructure:"name"`
+    OutputPaths       []string `json:"output-paths"       mapstructure:"output-paths"`
+    ErrorOutputPaths  []string `json:"error-output-paths" mapstructure:"error-output-paths"`
+    Level             string   `json:"level"              mapstructure:"level"`
+    Format            string   `json:"format"             mapstructure:"format"`
+    DisableCaller     bool     `json:"disable-caller"     mapstructure:"disable-caller"`
+    DisableStacktrace bool     `json:"disable-stacktrace" mapstructure:"disable-stacktrace"`
+    EnableColor       bool     `json:"enable-color"       mapstructure:"enable-color"`
+    Development       bool     `json:"development"        mapstructure:"development"`
+    Name              string   `json:"name"               mapstructure:"name"`
 }
 
 // NewOptions creates a Options object with default parameters.
 func NewOptions() *Options {
-	return &Options{
-		Level:             zapcore.InfoLevel.String(),
-		DisableCaller:     false,
-		DisableStacktrace: false,
-		Format:            consoleFormat,
-		EnableColor:       false,
-		Development:       false,
-		OutputPaths:       []string{"stdout"},
-		ErrorOutputPaths:  []string{"stderr"},
-	}
+    return &Options{
+        Level:             zapcore.InfoLevel.String(),
+        DisableCaller:     false,
+        DisableStacktrace: false,
+        Format:            consoleFormat,
+        EnableColor:       false,
+        Development:       false,
+        OutputPaths:       []string{"stdout"},
+        ErrorOutputPaths:  []string{"stderr"},
+    }
 }
 
 // Validate validate the options fields.
 func (o *Options) Validate() []error {
-	var errs []error
-
-	var zapLevel zapcore.Level
-	if err := zapLevel.UnmarshalText([]byte(o.Level)); err != nil {
-		errs = append(errs, err)
-	}
-
-	format := strings.ToLower(o.Format)
-	if format != consoleFormat && format != jsonFormat {
-		errs = append(errs, fmt.Errorf("not a valid kwlog format: %q", o.Format))
-	}
-
-	return errs
+    var errs []error
+    
+    var zapLevel zapcore.Level
+    if err := zapLevel.UnmarshalText([]byte(o.Level)); err != nil {
+        errs = append(errs, err)
+    }
+    
+    format := strings.ToLower(o.Format)
+    if format != consoleFormat && format != jsonFormat {
+        errs = append(errs, fmt.Errorf("not a valid kwlog format: %q", o.Format))
+    }
+    
+    return errs
 }
 
 // AddFlags adds flags for kwlog to the specified FlagSet object.
 func (o *Options) AddFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&o.Level, flagLevel, o.Level, "Minimum kwlog output `LEVEL`.")
-	fs.BoolVar(&o.DisableCaller, flagDisableCaller, o.DisableCaller, "Disable output of caller information in the kwlog.")
-	fs.BoolVar(&o.DisableStacktrace, flagDisableStacktrace,
-		o.DisableStacktrace, "Disable the kwlog to record a stack trace for all messages at or above panic level.")
-	fs.StringVar(&o.Format, flagFormat, o.Format, "Log output `FORMAT`, support plain or json format.")
-	fs.BoolVar(&o.EnableColor, flagEnableColor, o.EnableColor, "Enable output ansi colors in plain format logs.")
-	fs.StringSliceVar(&o.OutputPaths, flagOutputPaths, o.OutputPaths, "Output paths of kwlog.")
-	fs.StringSliceVar(&o.ErrorOutputPaths, flagErrorOutputPaths, o.ErrorOutputPaths, "Error output paths of kwlog.")
-	fs.BoolVar(
-		&o.Development,
-		flagDevelopment,
-		o.Development,
-		"Development puts the logger in development mode, which changes "+
-			"the behavior of DPanicLevel and takes stacktraces more liberally.",
-	)
-	fs.StringVar(&o.Name, flagName, o.Name, "The name of the logger.")
+    fs.StringVar(&o.Level, flagLevel, o.Level, "Minimum kwlog output `LEVEL`.")
+    fs.BoolVar(&o.DisableCaller, flagDisableCaller, o.DisableCaller, "Disable output of caller information in the kwlog.")
+    fs.BoolVar(&o.DisableStacktrace, flagDisableStacktrace,
+        o.DisableStacktrace, "Disable the kwlog to record a stack trace for all messages at or above panic level.")
+    fs.StringVar(&o.Format, flagFormat, o.Format, "Log output `FORMAT`, support plain or json format.")
+    fs.BoolVar(&o.EnableColor, flagEnableColor, o.EnableColor, "Enable output ansi colors in plain format logs.")
+    fs.StringSliceVar(&o.OutputPaths, flagOutputPaths, o.OutputPaths, "Output paths of kwlog.")
+    fs.StringSliceVar(&o.ErrorOutputPaths, flagErrorOutputPaths, o.ErrorOutputPaths, "Error output paths of kwlog.")
+    fs.BoolVar(
+        &o.Development,
+        flagDevelopment,
+        o.Development,
+        "Development puts the logger in development mode, which changes "+
+            "the behavior of DPanicLevel and takes stacktraces more liberally.",
+    )
+    fs.StringVar(&o.Name, flagName, o.Name, "The name of the logger.")
 }
 
 func (o *Options) String() string {
-	data, _ := json.Marshal(o)
-
-	return string(data)
+    data, _ := json.Marshal(o)
+    
+    return string(data)
 }
 
 // Build constructs a global zap logger from the Config and Options.
 func (o *Options) Build() error {
-	var zapLevel zapcore.Level
-	if err := zapLevel.UnmarshalText([]byte(o.Level)); err != nil {
-		zapLevel = zapcore.InfoLevel
-	}
-	encodeLevel := zapcore.CapitalLevelEncoder
-	if o.Format == consoleFormat && o.EnableColor {
-		encodeLevel = zapcore.CapitalColorLevelEncoder
-	}
-
-	zc := &zap.Config{
-		Level:             zap.NewAtomicLevelAt(zapLevel),
-		Development:       o.Development,
-		DisableCaller:     o.DisableCaller,
-		DisableStacktrace: o.DisableStacktrace,
-		Sampling: &zap.SamplingConfig{
-			Initial:    100,
-			Thereafter: 100,
-		},
-		Encoding: o.Format,
-		EncoderConfig: zapcore.EncoderConfig{
-			MessageKey:     "message",
-			LevelKey:       "level",
-			TimeKey:        "timestamp",
-			NameKey:        "logger",
-			CallerKey:      "caller",
-			StacktraceKey:  "stacktrace",
-			LineEnding:     zapcore.DefaultLineEnding,
-			EncodeLevel:    encodeLevel,
-			EncodeTime:     timeEncoder,
-			EncodeDuration: milliSecondsDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder,
-			EncodeName:     zapcore.FullNameEncoder,
-		},
-		OutputPaths:      o.OutputPaths,
-		ErrorOutputPaths: o.ErrorOutputPaths,
-	}
-	logger, err := zc.Build(zap.AddStacktrace(zapcore.PanicLevel))
-	if err != nil {
-		return err
-	}
-	zap.RedirectStdLog(logger.Named(o.Name))
-	zap.ReplaceGlobals(logger)
-
-	return nil
+    var zapLevel zapcore.Level
+    if err := zapLevel.UnmarshalText([]byte(o.Level)); err != nil {
+        zapLevel = zapcore.InfoLevel
+    }
+    encodeLevel := zapcore.CapitalLevelEncoder
+    if o.Format == consoleFormat && o.EnableColor {
+        encodeLevel = zapcore.CapitalColorLevelEncoder
+    }
+    
+    zc := &zap.Config{
+        Level:             zap.NewAtomicLevelAt(zapLevel),
+        Development:       o.Development,
+        DisableCaller:     o.DisableCaller,
+        DisableStacktrace: o.DisableStacktrace,
+        Sampling: &zap.SamplingConfig{
+            Initial:    100,
+            Thereafter: 100,
+        },
+        Encoding: o.Format,
+        EncoderConfig: zapcore.EncoderConfig{
+            MessageKey:     "message",
+            LevelKey:       "level",
+            TimeKey:        "timestamp",
+            NameKey:        "logger",
+            CallerKey:      "caller",
+            StacktraceKey:  "stacktrace",
+            LineEnding:     zapcore.DefaultLineEnding,
+            EncodeLevel:    encodeLevel,
+            EncodeTime:     timeEncoder,
+            EncodeDuration: milliSecondsDurationEncoder,
+            EncodeCaller:   zapcore.ShortCallerEncoder,
+            EncodeName:     zapcore.FullNameEncoder,
+        },
+        OutputPaths:      o.OutputPaths,
+        ErrorOutputPaths: o.ErrorOutputPaths,
+    }
+    logger, err := zc.Build(zap.AddStacktrace(zapcore.PanicLevel))
+    if err != nil {
+        return err
+    }
+    zap.RedirectStdLog(logger.Named(o.Name))
+    zap.ReplaceGlobals(logger)
+    
+    return nil
 }
