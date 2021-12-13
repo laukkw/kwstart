1bd6dc74021e04f5f3a158f15498802b0cec3417
rzry <1319339638@qq.com>
2021-12-13 17:54:00 +0800

    add:db log interface
    Signed-off-by: rzry <1319339638@qq.com>

--- kwapp/help.go
+++ kwapp/help.go
@@ -1,57 +1,53 @@
-// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
-
 package kwapp
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/fatih/color"
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
+    "fmt"
+    "strings"
+    
+    "github.com/fatih/color"
+    "github.com/spf13/cobra"
+    "github.com/spf13/pflag"
 )
 
 const (
-	flagHelp          = "help"
-	flagHelpShorthand = "H"
+    flagHelp          = "help"
+    flagHelpShorthand = "H"
 )
 
 func helpCommand(name string) *cobra.Command {
-	return &cobra.Command{
-		Use:   "help [command]",
-		Short: "Help about any command.",
-		Long: `Help provides help for any command in the application.
+    return &cobra.Command{
+        Use:   "help [command]",
+        Short: "Help about any command.",
+        Long: `Help provides help for any command in the application.
 Simply type ` + name + ` help [path to command] for full details.`,
-
-		Run: func(c *cobra.Command, args []string) {
-			cmd, _, e := c.Root().Find(args)
-			if cmd == nil || e != nil {
-				c.Printf("Unknown help topic %#q\n", args)
-				_ = c.Root().Usage()
-			} else {
-				cmd.InitDefaultHelpFlag() // make possible 'help' flag to be shown
-				_ = cmd.Help()
-			}
-		},
-	}
+        
+        Run: func(c *cobra.Command, args []string) {
+            cmd, _, e := c.Root().Find(args)
+            if cmd == nil || e != nil {
+                c.Printf("Unknown help topic %#q\n", args)
+                _ = c.Root().Usage()
+            } else {
+                cmd.InitDefaultHelpFlag() // make possible 'help' flag to be shown
+                _ = cmd.Help()
+            }
+        },
+    }
 }
 
 // addHelpFlag adds flags for a specific application to the specified FlagSet
 // object.
 //nolint: deadcode,unused,varcheck
 func addHelpFlag(name string, fs *pflag.FlagSet) {
-	fs.BoolP(flagHelp, flagHelpShorthand, false, fmt.Sprintf("Help for %s.", name))
+    fs.BoolP(flagHelp, flagHelpShorthand, false, fmt.Sprintf("Help for %s.", name))
 }
 
 // addHelpCommandFlag adds flags for a specific command of application to the
 // specified FlagSet object.
 func addHelpCommandFlag(usage string, fs *pflag.FlagSet) {
-	fs.BoolP(
-		flagHelp,
-		flagHelpShorthand,
-		false,
-		fmt.Sprintf("Help for the %s command.", color.GreenString(strings.Split(usage, " ")[0])),
-	)
+    fs.BoolP(
+        flagHelp,
+        flagHelpShorthand,
+        false,
+        fmt.Sprintf("Help for the %s command.", color.GreenString(strings.Split(usage, " ")[0])),
+    )
 }
--- kwdb/mysql.go
+++ kwdb/mysql.go
@@ -1,57 +1,57 @@
 package kwdb
 
 import (
-	"fmt"
-	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-
-	"github.com/rzry/kwstart/kwlog/logger"
+    "fmt"
+    "time"
+    
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+    "gorm.io/gorm/logger"
 )
 
 // Options defines optsions for mysql database.
 type Options struct {
-	Host                  string
-	Username              string
-	Password              string
-	Database              string
-	MaxIdleConnections    int
-	MaxOpenConnections    int
-	MaxConnectionLifeTime time.Duration
-	LogLevel              int
+    Host                  string
+    Username              string
+    Password              string
+    Database              string
+    MaxIdleConnections    int
+    MaxOpenConnections    int
+    MaxConnectionLifeTime time.Duration
+    LogLevel              int
+    Logger                logger.Interface
 }
 
 // New create a new gorm db instance with the given options.
 func New(opts *Options) (*gorm.DB, error) {
-	dns := fmt.Sprintf(`%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s`,
-		opts.Username,
-		opts.Password,
-		opts.Host,
-		opts.Database,
-		true,
-		"Local")
-
-	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{
-		Logger: logger.New(opts.LogLevel),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	sqlDB, err := db.DB()
-	if err != nil {
-		return nil, err
-	}
-
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	sqlDB.SetMaxOpenConns(opts.MaxOpenConnections)
-
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-	sqlDB.SetConnMaxLifetime(opts.MaxConnectionLifeTime)
-
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	sqlDB.SetMaxIdleConns(opts.MaxIdleConnections)
-
-	return db, nil
+    dns := fmt.Sprintf(`%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s`,
+        opts.Username,
+        opts.Password,
+        opts.Host,
+        opts.Database,
+        true,
+        "Local")
+    
+    db, err := gorm.Open(mysql.Open(dns), &gorm.Config{
+        Logger: opts.Logger,
+    })
+    if err != nil {
+        return nil, err
+    }
+    
+    sqlDB, err := db.DB()
+    if err != nil {
+        return nil, err
+    }
+    
+    // SetMaxOpenConns sets the maximum number of open connections to the database.
+    sqlDB.SetMaxOpenConns(opts.MaxOpenConnections)
+    
+    // SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+    sqlDB.SetConnMaxLifetime(opts.MaxConnectionLifeTime)
+    
+    // SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+    sqlDB.SetMaxIdleConns(opts.MaxIdleConnections)
+    
+    return db, nil
 }
